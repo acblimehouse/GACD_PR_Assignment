39eeee96f6222a02f32309c12652c97d1ebb34a9
acblimehouse <abriskin-limehouse@optimalsolutionsgroup.com>
2017-04-23 19:23:03 -0700

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,4 @@
+## Set 
+mydir <- "/Users/adamlimehouse/Desktop/Dropbox/03 Projects Folder/Economic and Policy Analysis/Intro to Data Science R Files/GACDPeerReview_20160423/GACD_PR_Assignment"
+setwd(mydir) ## Set mydir to your preferred local directory.
+
